be4279f2b88e4810f3248ba6c6a0e687224218f7
hakunaconsultant <94793378+hakunaconsultant@users.noreply.github.com>
2022-04-02 17:05:52 +0530

    simple linear

---
+++ simple linear relation.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 21 22:54:43 2022
+
+@author: HP
+"""
+# Simple Linear Regression
+
+# Importing the libraries
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('C:/Users/HP/Downloads/stud_reg.csv')
+print(type(dataset))
+
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+ 
+    
+# Fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#Calculating the coefficients:
+print(regressor.coef_)
+
+#Calculating the intercept:
+print(regressor.intercept_)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+# Accuracy of the model
+
+#Calculating the r squared value:
+from sklearn.metrics import r2_score
+r2_score(y_test,y_pred)
+
+#Create a DataFrame
+df1 = {'Actual Applicants':y_test,
+'Predicted Applicants':y_pred}
+df1 = pd.DataFrame(df1,columns=['Actual Applicants','Predicted Applicants'])
+print(df1)
+ 
+# Visualising the predicted results
+line_chart1 = plt.plot(X_test,y_pred, '--', c ='red')
+line_chart2 = plt.plot(X_test,y_test, ':', c='blue')
+
